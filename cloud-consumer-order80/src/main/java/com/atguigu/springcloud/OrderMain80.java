7b809dd75c2df5274f1eee04026674d0d5f368c3
Athletic <859205795@qq.com>
2022-01-20 22:52:19 +0800

    update

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -16,7 +16,9 @@ import org.springframework.cloud.netflix.ribbon.RibbonClients;
 @EnableEurekaClient
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration=MySelfRule.class)
 public class OrderMain80 {
+
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
     }
+
 }
