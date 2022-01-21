26d6c2fe177aecd630a50c3431415522b9f1c9ab
Athletic <859205795@qq.com>
2022-01-21 17:43:06 +0800

    update

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,17 +1,24 @@
 package com.atguigu.springcloud.controller;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @auther zzyy
@@ -37,7 +44,7 @@ public class PaymentController {
         log.info("*****插入结果："+result);
 
         if(result > 0) {
-            return new CommonResult(200,"插入数据库成功,serverPort: " + serverPort, result);
+            return new CommonResult(200,"插入数据库成功, serverPort: " + serverPort, result);
         }else {
             return new CommonResult(444,"插入数据库失败",null);
         }
@@ -48,9 +55,9 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
 
         if(payment != null) {
-            return new CommonResult(200,"查询成功,serverPort:  " + serverPort, payment);
+            return new CommonResult(200,"查询成功, serverPort:  " + serverPort, payment);
         }else {
-            return new CommonResult(444,"没有对应记录,查询ID: " + id,null);
+            return new CommonResult(444,"没有对应记录, 查询ID: " + id,null);
         }
     }
 
