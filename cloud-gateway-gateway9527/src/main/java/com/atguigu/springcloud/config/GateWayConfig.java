5fb813fd9bfe13e5d06d0b790ae700ef39232a20
Athletic <859205795@qq.com>
2022-01-23 16:04:55 +0800

    update

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -6,17 +6,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 自定义路由
  * @auther zzyy
  * @create 2020-02-21 11:42
  */
 @Configuration
-public class GateWayConfig
-{
+public class GateWayConfig {
+
+    /**
+     * 配置了一个 id 为 route-name 的路由规则，
+     * 当访问地址 http://localhost:9527/guonei 时会自动转发到地址: http://news.baidu.com/guonei
+     */
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
-    {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
 
+        //现在访问localhost的/guonei的地址将会转发到"http://news.baidu.com/guonei"这个地址
         routes.route("path_route_atguigu",
                 r -> r.path("/guonei")
                         .uri("http://news.baidu.com/guonei")).build();
--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGateWayFilter.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGateWayFilter.java
@@ -1,12 +1,14 @@
 package com.atguigu.springcloud.filter;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+
 import reactor.core.publisher.Mono;
 
 import java.util.Date;
@@ -17,18 +19,15 @@ import java.util.Date;
  */
 @Component
 @Slf4j
-public class MyLogGateWayFilter implements GlobalFilter,Ordered
-{
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
-    {
-        log.info("***********come in MyLogGateWayFilter:  "+new Date());
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("***********come in MyLogGateWayFilter:  " + new Date());
 
         String uname = exchange.getRequest().getQueryParams().getFirst("uname");
 
-        if(uname == null)
-        {
+        if (uname == null) {
             log.info("*******用户名为null，非法用户，o(╥﹏╥)o");
             exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
             return exchange.getResponse().setComplete();
@@ -38,8 +37,8 @@ public class MyLogGateWayFilter implements GlobalFilter,Ordered
     }
 
     @Override
-    public int getOrder()
-    {
+    public int getOrder() {
         return 0;
     }
+
 }
