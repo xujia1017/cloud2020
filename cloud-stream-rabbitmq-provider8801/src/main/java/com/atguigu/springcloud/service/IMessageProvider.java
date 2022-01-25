b1d841063772003854802939e10f7af7404994cb
Athletic <859205795@qq.com>
2022-01-25 16:26:29 +0800

    update

--- cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
@@ -4,7 +4,11 @@ package com.atguigu.springcloud.service;
  * @auther zzyy
  * @create 2020-02-22 10:55
  */
-public interface IMessageProvider
-{
-    public String send();
+public interface IMessageProvider {
+
+    /**
+     * 定义消息的发送接口
+     */
+    String send();
+
 }
--- cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
@@ -1,32 +1,37 @@
 package com.atguigu.springcloud.service.impl;
 
-import com.atguigu.springcloud.service.IMessageProvider;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.integration.support.MessageBuilderFactory;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.integration.support.MessageBuilder;
-import javax.annotation.Resource;
-import org.springframework.cloud.stream.messaging.Source;
-
-import javax.annotation.Resource;
 import java.util.UUID;
 
+import javax.annotation.Resource;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+
 /**
+ * 定义消息生产者的的推送管道
+ *
  * @auther zzyy
  * @create 2020-02-22 10:56
  */
-@EnableBinding(Source.class) //定义消息的推送管道
-public class MessageProviderImpl implements IMessageProvider
-{
+@EnableBinding(Source.class)
+public class MessageProviderImpl implements IMessageProvider {
+
+    /**
+     * 消息发送管道
+     */
     @Resource
-    private MessageChannel output; // 消息发送管道
+    private MessageChannel output;
 
     @Override
-    public String send()
-    {
+    public String send() {
         String serial = UUID.randomUUID().toString();
         output.send(MessageBuilder.withPayload(serial).build());
-        System.out.println("*****serial: "+serial);
+        System.out.println("*****serial: " + serial);
         return null;
     }
+
 }
