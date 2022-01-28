85f6e25681e82a75acba1e0b82356a7e304ad4ba
Athletic <859205795@qq.com>
2022-01-28 16:06:48 +0800

    update

--- cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -10,9 +10,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class NacosConfigClientMain3377
-{
+public class NacosConfigClientMain3377 {
+
     public static void main(String[] args) {
         SpringApplication.run(NacosConfigClientMain3377.class, args);
     }
+
 }
