a6818b1972cd4afeb958d3bd51996a582c4f832b
Athletic <859205795@qq.com>
2022-02-12 17:21:27 +0800

    update

--- cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
+ * Nacos: 前四个字母分别是Naming和Configuration的前两个字母，最后的s为service
+ *      Nacos就是注册中心+配置中心
+ *      Nacos = Eureka + Config + Bus
+ *      替代Eureka做服务注册中心，替代Config做服务配置中心
+ *
  * @auther zzyy
  * @create 2020-02-23 17:01
  */
