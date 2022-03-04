750a98d4b5ed632af1a78385ba75ff0bb69d99ae
Athletic <859205795@qq.com>
2022-03-04 16:02:22 +0800

    update

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -28,6 +28,27 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PaymentController {
 
+
+    /**
+     * @RestController:
+     *      这个注解等效于 @Controller 加上 @ResponseBody, 添加了这个注解就是让这个类返回json串，这是spring内部提供的json解析。
+     *
+     * @Resource:
+     *      默认按照名称进行装配，名称可以通过name属性进行指定，如果没有指定name属性，当注解写在字段上时，默认取字段名进行安装名称查找，
+     *      如果注解写在setter方法上默认取属性名进行装配。当找不到与名称匹配的bean时才按照类型进行装配。
+     *      但是需要注意的是，如果name属性一旦指定，就只会按照名称进行装配。这个注解属于J2EE的。
+     *
+     * @Autowired:
+     *      默认按类型装配, 默认情况下必须要求依赖对象必须存在, 如果要允许null值, 可以设置它的required属性为false, 如：@Autowired(required=false),
+     *      这个注解是属于spring的，如果我们想使用名称装配可以结合 @Qualifier 注解进行使用。
+     *
+     * @RequesMapping:
+     *      注解是一个地址映射的注解。就是根据这个地址，可以找到这个方法，这个类，注解到类上，就相当于方法的父类地址。
+     *
+     * @Value:
+     *      该注解的作用就是将我们配置文件的属性值读出来，有 @Value("${}") 和 @Value("#{}")两种方式。
+     */
+
     @Resource
     private PaymentService paymentService;
 
